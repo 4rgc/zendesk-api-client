2da81b9134db9f7b6a7a640066c4258e9794579d
4rgc <bohdan.andrii@gmail.com>
2021-11-29 23:41:20 -0500

    fix: remove zendeskClient middleware

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import { zendeskClient } from './middleware/zendeskClient';
 dotenv.config();
 
 import ticketsRouter from './routes/tickets';
@@ -9,7 +8,6 @@ import ticketsRouter from './routes/tickets';
 const app = express();
 
 app.use(cors());
-app.use(zendeskClient);
 
 app.get('/', (req, res) => {
 	res.send('Hello');
